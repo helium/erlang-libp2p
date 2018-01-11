bd074977f5298ba730e127db569a9923d79067d3
Marc Nijdam <marc@helium.com>
2018-01-11 10:06:52 -0800

    Add some basic unit tests for multistream_client

---
+++ test/multistream_test.erl
@@ -0,0 +1,26 @@
+-module(multistream_test).
+
+-include_lib("eunit/include/eunit.hrl").
+
+client_ls_test() ->
+    Swarms = [S1] = test_util:setup_swarms(1),
+    [Addr] = libp2p_swarm:listen_addrs(S1),
+
+    {ok, Connection} = libp2p_transport_tcp:dial(Addr),
+
+    ?assertEqual(ok, libp2p_multistream_client:handshake(Connection)),
+    ?assertMatch(["yamux/1.0.0" | _], libp2p_multistream_client:ls(Connection)),
+    ?assertEqual(ok, libp2p_multistream_client:select("yamux/1.0.0", Connection)),
+
+    test_util:teardown_swarms(Swarms).
+
+client_negotiate_handler_test() ->
+    Swarms = [S1] = test_util:setup_swarms(1),
+    [Addr] = libp2p_swarm:listen_addrs(S1),
+
+    {ok, Connection} = libp2p_transport_tcp:dial(Addr),
+
+    Handlers = [{"othermux", "othermux"}, {"yamux/1.0.0", "yamux"}],
+    ?assertEqual({ok, "yamux"}, libp2p_multistream_client:negotiate_handler(Handlers, "", Connection)),
+
+    test_util:teardown_swarms(Swarms).
--- test/test_util.erl
+++ test/test_util.erl
@@ -1,14 +1,18 @@
 -module(test_util).
 
--export([setup_swarms/0, teardown_swarms/1]).
+-export([setup_swarms/0, setup_swarms/1, teardown_swarms/1]).
+
+setup_swarms(0, Acc) ->
+    Acc;
+setup_swarms(N, Acc) ->
+    setup_swarms(N - 1, [libp2p_swarm:start(0) | Acc]).
+
+setup_swarms(N) when N >= 1 ->
+    application:ensure_all_started(ranch),
+    setup_swarms(N, []).
 
 setup_swarms() ->
-    application:ensure_all_started(ranch),
-    %% application:ensure_all_started(lager),
-    %% lager:set_loglevel(lager_console_backend, debug),
-    S1 = libp2p_swarm:start(0),
-    S2 = libp2p_swarm:start(0),
-    [S1, S2].
+    setup_swarms(2).
 
 teardown_swarms(Swarms) ->
     lists:map(fun libp2p_swarm:stop/1, Swarms).
