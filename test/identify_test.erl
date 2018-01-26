8fb98adc09bd28403429b686198e54c29f0417f7
Andrew Thompson <andrew@hijacked.us>
2018-01-26 08:56:54 -0800

    Fix test failures when test host is multi-homed

--- test/identify_test.erl
+++ test/identify_test.erl
@@ -5,14 +5,14 @@
 
 identify_test() ->
     Swarms = [S1, S2] = test_util:setup_swarms(),
-    [S2Addr] = libp2p_swarm:listen_addrs(S2),
+    [S2Addr|_] = libp2p_swarm:listen_addrs(S2),
 
     {ok, Stream} = libp2p_swarm:dial(S1, S2Addr, "identify/1.0.0"),
     {S1Addr, _} = libp2p_connection:addr_info(Stream),
     {ok, IdentifyBin} = libp2p_framed_stream:recv(Stream),
     Identify = libp2p_identify:decode(IdentifyBin),
     ?assertEqual("identify/1.0.0", libp2p_identify:protocol_version(Identify)),
-    ?assertEqual([multiaddr:new(S2Addr)], libp2p_identify:listen_addrs(Identify)),
+    ?assert(lists:member(multiaddr:new(S2Addr), libp2p_identify:listen_addrs(Identify))),
     ?assertEqual(multiaddr:new(S1Addr), libp2p_identify:observed_addr(Identify)),
     ?assertMatch(("erlang-libp2p/" ++  _), libp2p_identify:agent_version(Identify)),
 
