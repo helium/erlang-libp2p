58a3a8d7434250a56b7d46cfa5cdf2e708e38f49
macpie <macpie@users.noreply.github.com>
2018-06-12 11:26:07 -0700

    HELIUM-293: Allow to disable nat for tests (#62)

--- test/test_util.erl
+++ test/test_util.erl
@@ -20,7 +20,8 @@ setup_swarms(N, Opts, Acc) ->
                       Name = list_to_atom("swarm" ++ integer_to_list(erlang:unique_integer([monotonic]))),
                       TmpDir = lib:nonl(os:cmd("mktemp -d")),
                       BaseDir = libp2p_config:get_opt(Opts, base_dir, TmpDir),
-                      NewOpts = lists:keystore(base_dir, 1, Opts, {base_dir, BaseDir}),
+                      NewOpts = lists:keystore(base_dir, 1, Opts, {base_dir, BaseDir})
+                        ++ [{libp2p_transport_tcp, [{nat, false}]}],
                       {ok, Pid} = libp2p_swarm:start(Name, NewOpts),
                       ok = libp2p_swarm:listen(Pid, "/ip4/0.0.0.0/tcp/0"),
                       Pid
