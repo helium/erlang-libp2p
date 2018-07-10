90e5e6791d3abbcb03582edb0c1d7a2ea8351d0a
macpie <pierre@helium.com>
2018-07-10 15:26:20 -0700

    HELIUM-30: Add dummy frame stream

--- test/relay_SUITE.erl
+++ test/relay_SUITE.erl
@@ -61,24 +61,44 @@ basic(_Config) ->
     {ok, BSwarm} = libp2p_swarm:start(b, SwarmOpts),
     {ok, RSwarm} = libp2p_swarm:start(r, SwarmOpts),
 
-    ok = libp2p_swarm:listen(ASwarm, "/ip4/0.0.0.0/tcp/0"),
-    ok = libp2p_swarm:listen(BSwarm, "/ip4/0.0.0.0/tcp/0"),
-    ok = libp2p_swarm:listen(RSwarm, "/ip4/0.0.0.0/tcp/0"),
+    ok = libp2p_swarm:listen(ASwarm, "/ip4/0.0.0.0/tcp/6600"),
+    ok = libp2p_swarm:listen(BSwarm, "/ip4/0.0.0.0/tcp/6601"),
+    ok = libp2p_swarm:listen(RSwarm, "/ip4/0.0.0.0/tcp/6602"),
 
+    Version = "relaytest/1.0.0",
+    libp2p_swarm:add_stream_handler(
+        ASwarm
+        ,Version
+        ,{libp2p_framed_stream, server, [libp2p_stream_relay_test, self(), ASwarm]}
+    ),
+    libp2p_swarm:add_stream_handler(
+        BSwarm
+        ,Version
+        ,{libp2p_framed_stream, server, [libp2p_stream_relay_test, self(), BSwarm]}
+    ),
+    libp2p_swarm:add_stream_handler(
+        RSwarm
+        ,Version
+        ,{libp2p_framed_stream, server, [libp2p_stream_relay_test, self(), RSwarm]}
+    ),
     [RAddress] = libp2p_swarm:listen_addrs(RSwarm),
 
-    % A dials R
-    libp2p_relay:dial(ASwarm, RAddress, []),
+    % NAT fails so A dials R to create a relay
+    {ok, _} = libp2p_relay:dial(ASwarm, RAddress, []),
     % Waiting for connection
     timer:sleep(2000),
 
+    % Testing relay address
     % Once relay is established get relay address from A's peerbook
     [_, ARelayAddress|_] = libp2p_swarm:listen_addrs(ASwarm),
-    [A, _B] = string:split(ARelayAddress, "/p2p-circuit"),
-    lager:warning("[~p:~p:~p] MARKER ~p~n", [?MODULE, ?FUNCTION_NAME, ?LINE, 1]),
     % B dials A via the relay address (so dialing R realy)
-    {ok, Conn} = libp2p_relay:dial(BSwarm, A, [{relay, ARelayAddress}]),
-    lager:warning("[~p:~p:~p] MARKER ~p~n", [?MODULE, ?FUNCTION_NAME, ?LINE, Conn]),
+    {ok, _} = case libp2p_swarm:dial(BSwarm, ARelayAddress, Version) of
+        {ok, Conn} ->
+            libp2p_framed_stream:client(libp2p_stream_relay_test, Conn, []);
+        Error ->
+            ct:fail(Error)
+    end,
+
     % libp2p_framed_stream:client(libp2p_stream_relay, Conn, [{swarm, BSwarm}]),
     timer:sleep(2000),
 
