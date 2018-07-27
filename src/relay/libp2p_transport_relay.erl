41f2c43da1d40c9d9441ddcdbd55b8c7b7b6a067
macpie <pierre@helium.com>
2018-07-26 17:04:44 -0700

    HELIUM-30: relay transport not locked to transport tcp anymore

--- src/relay/libp2p_transport_relay.erl
+++ src/relay/libp2p_transport_relay.erl
@@ -27,23 +27,23 @@ match_addr(Addr) when is_list(Addr) ->
 
 -spec connect(pid(), string(), libp2p_swarm:connect_opts()
               ,pos_integer(), ets:tab()) -> {ok, pid()} | {error, term()}.
-connect(Pid, MAddr, Options, Timeout, TID) ->
-    {ok, {To, A}} = libp2p_relay:p2p_circuit(MAddr),
-    true = erlang:register(?MODULE:reg(A), self()),
-    % TODO: This should not be forced to tcp will have to find a fix for that
-    case libp2p_transport_tcp:connect(Pid, To, Options, Timeout, TID) of
-        {error, _Reason}=Error -> Error;
+connect(_Pid, MAddr, Options, Timeout, TID) ->
+    {ok, {RAddress, AAddress}} = libp2p_relay:p2p_circuit(MAddr),
+    true = erlang:register(?MODULE:reg(AAddress), self()),
+    case libp2p_transport:connect_to(RAddress, Options, Timeout, TID) of
+        {error, _Reason}=Error ->
+            Error;
         {ok, _SessionPid} ->
             Swarm = libp2p_swarm:swarm(TID),
             {ok, _} = libp2p_relay:dial_framed_stream(
                 Swarm
-                ,To
+                ,RAddress
                 ,[{type, {bridge_br, MAddr}}]
             ),
             receive
                 {sessions, [SessionPid|_]=Sessions} ->
                     lager:info("using sessions: ~p instead of ~p", [Sessions, _SessionPid]),
-                    true  = erlang:unregister(?MODULE:reg(A)),
+                    true  = erlang:unregister(?MODULE:reg(AAddress)),
                     {ok, SessionPid};
                 _Error ->
                     lager:error("no relay sessions ~p", [_Error]),
