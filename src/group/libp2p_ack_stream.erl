c999dd1e603924aa3dfa6b4531e5ce09cf5c282d
Evan Vigil-McClanahan <evan@helium.com>
2018-12-21 17:03:11 -0800

    wip

--- src/group/libp2p_ack_stream.erl
+++ src/group/libp2p_ack_stream.erl
@@ -67,6 +67,7 @@ handle_data(_Kind, Data, State=#state{ack_ref=AckRef, ack_module=AckModule, ack_
     end.
 
 handle_send(_Kind, From, Data, Timeout, State=#state{}) ->
+    ct:pal("send ~p ~p ~p", [_Kind, From, Data]),
     Msg = #libp2p_ack_frame_pb{frame={data, Data}},
     {ok, {reply, From, pending}, libp2p_ack_stream_pb:encode_msg(Msg), Timeout, State#state{}}.
 
