2c4beb23ed1273ca5a76c655bfd3203ae6f9804c
Marc Nijdam <marc@helium.com>
2019-04-06 17:30:02 -0700

    Simpler?

--- ci/build.sh
+++ ci/build.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-set -ex
-
+set -e
 
 . "$HOME/.asdf/asdf.sh"
 asdf local erlang 21.3
 
 (
+    set -x
     echo --- cleaning build
     make clean
     echo --- build
-    make 2>&1 | tee build.log | sed 's/^\(\x1b\[[0-9;]*m\)*>>>/---/'
+    ./rebar3 as test do eunit,ct && ./rebar3 dialyzer 2>&1 | tee build.log | sed 's/^\(\x1b\[[0-9;]*m\)*>>>/---/'
 )
 
 echo --- ok
