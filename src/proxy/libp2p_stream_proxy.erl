73d4dedd8cbbfc5cb41c3d2a6fba4d678dd036b7
Andrew Thompson <andrew@hijacked.us>
2019-07-19 10:01:34 -0700

    Obtain a new relay address on overload

--- src/proxy/libp2p_stream_proxy.erl
+++ src/proxy/libp2p_stream_proxy.erl
@@ -155,9 +155,10 @@ handle_server_data({overload, Overload}, _Env, #state{swarm=Swarm}=State) ->
     R = libp2p_crypto:pubkey_bin_to_p2p(PubKeyBin),
     A = libp2p_swarm:p2p_address(Swarm),
     P2PCircuit = libp2p_relay:p2p_circuit(R, A),
-    lager:info("proxy server was overloaded removing address ~p", [P2PCircuit]),
-    TID = libp2p_swarm:tid(Swarm),
-    true = libp2p_config:remove_listener(TID, P2PCircuit),
+    lager:info("proxy server was overloaded, removing address: ~p", [P2PCircuit]),
+    %% stop and start the relay to try to obtain a new peer
+    libp2p_relay_server:stop(Swarm),
+    libp2p_relay_server:relay(Swarm),
     {stop, normal, State};
 handle_server_data(_Data, _Env, State) ->
     lager:warning("server unknown envelope ~p", [_Env]),
