f24f7ba90d9be3cd721ae5e90b461ee8989c7eac
macpie <pierre@helium.com>
2019-08-27 10:38:02 -0700

    Cache docs

--- src/libp2p_cache.erl
+++ src/libp2p_cache.erl
@@ -1,6 +1,7 @@
 %%%-------------------------------------------------------------------
 %% @doc
 %% == Libp2p Cache ==
+%% Simple dets cache
 %% @end
 %%%-------------------------------------------------------------------
 -module(libp2p_cache).
@@ -40,24 +41,43 @@
 %% ------------------------------------------------------------------
 %% API Function Definitions
 %% ------------------------------------------------------------------
+
+%% @hidden
 start_link(Args) ->
     gen_server:start_link(?MODULE, [Args], []).
 
-
+%%--------------------------------------------------------------------
+%% @doc
+%% Insert key, value in cache
+%% @end
+%%--------------------------------------------------------------------
 -spec insert(pid(), any(), any()) -> ok | {error, any()}.
 insert(Pid, Key, Value) ->
     gen_server:call(Pid, {insert, Key, Value}).
 
-
+%%--------------------------------------------------------------------
+%% @doc
+%% Lookup key in cache (default to `undefined')
+%% @end
+%%--------------------------------------------------------------------
 -spec lookup(pid(), any()) -> undefined | any().
 lookup(Pid, Key) ->
     lookup(Pid, Key, undefined).
 
+%%--------------------------------------------------------------------
+%% @doc
+%% Lookup key in cache with default value
+%% @end
+%%--------------------------------------------------------------------
 -spec lookup(pid(), any(), any()) -> undefined | any().
 lookup(Pid, Key, Default) ->
     gen_server:call(Pid, {lookup, Key, Default}).
 
-
+%%--------------------------------------------------------------------
+%% @doc
+%% Delete key in cache
+%% @end
+%%--------------------------------------------------------------------
 -spec delete(pid(), any()) -> ok | {error, any()}.
 delete(Pid, Key) ->
     gen_server:call(Pid, {delete, Key}).
@@ -65,6 +85,8 @@ delete(Pid, Key) ->
 %% ------------------------------------------------------------------
 %% gen_server Function Definitions
 %% ------------------------------------------------------------------
+
+%% @hidden
 init([TID]) ->
     erlang:process_flag(trap_exit, true),
     libp2p_swarm_auxiliary_sup:register_cache(TID),
@@ -75,6 +97,7 @@ init([TID]) ->
     _ = migrate(Dets),
     {ok, #state{dets=Dets}}.
 
+%% @hidden
 handle_call({insert, Key, Value}, _From, #state{dets=Dets}=State) ->
     Result = dets:insert(Dets, {Key, Value}),
     {reply, Result, State};
@@ -92,17 +115,21 @@ handle_call(_Msg, _From, State) ->
     lager:warning("rcvd unknown call msg: ~p from: ~p", [_Msg, _From]),
     {reply, ok, State}.
 
+%% @hidden
 handle_cast(_Msg, State) ->
     lager:warning("rcvd unknown cast msg: ~p", [_Msg]),
     {noreply, State}.
 
+%% @hidden
 handle_info(_Msg, State) ->
     lager:warning("rcvd unknown info msg: ~p", [_Msg]),
     {noreply, State}.
 
+%% @hidden
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
+%% @hidden
 terminate(_Reason, #state{dets=Name}) ->
     ok = dets:close(Name),
     ok.
