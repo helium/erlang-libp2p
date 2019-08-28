08795f7daa84a22d964a6883506cba8cd46b3fdc
Evan Vigil-McClanahan <evan@helium.com>
2019-08-28 06:43:17 -0700

    cleanup ets table and remove trap exit (#208)
    * remove trap-exit
    * cleanup dead sessions in ets

--- src/libp2p_swarm_session_sup.erl
+++ src/libp2p_swarm_session_sup.erl
@@ -14,9 +14,14 @@ start_link(TID) ->
 
 init([TID]) ->
     _ = ets:insert(TID, {?SUP, self()}),
+    _ = cleanup_ets(TID),
     SupFlags = #{ strategy  => one_for_one},
     {ok, {SupFlags, []}}.
 
 -spec sup(ets:tab()) -> pid().
 sup(TID) ->
     ets:lookup_element(TID, ?SUP, 2).
+
+cleanup_ets(TID) ->
+    DeadSessions = lists:filter(fun({_, P}) -> not erlang:is_process_alive(P) end, libp2p_config:lookup_sessions(TID)),
+    [libp2p_config:remove_pid(TID, Pid) || {_, Pid} <- DeadSessions].
