ebda1b1cd032dcc1c00382804858dea095f3ae3c
macpie <pierre@helium.com>
2019-09-09 15:21:42 -0700

    Add identify doc

--- src/identify/libp2p_stream_identify.erl
+++ src/identify/libp2p_stream_identify.erl
@@ -1,11 +1,24 @@
+%%%-------------------------------------------------------------------
+%% @doc
+%% == Libp2p identify Stream ==
+%% @see libp2p_framed_stream
+%% @end
+%%%-------------------------------------------------------------------
 -module(libp2p_stream_identify).
-
--include("pb/libp2p_identify_pb.hrl").
 
 -behavior(libp2p_framed_stream).
 
--export([dial_spawn/3]).
--export([client/2, server/4, init/3, handle_data/3, handle_info/3]).
+%% ------------------------------------------------------------------
+%% API Function Exports
+%% ------------------------------------------------------------------
+-export([client/2, server/4, dial_spawn/3]).
+
+%% ------------------------------------------------------------------
+%% libp2p_framed_stream Function Exports
+%% ------------------------------------------------------------------
+-export([init/3, handle_data/3, handle_info/3]).
+
+-include("pb/libp2p_identify_pb.hrl").
 
 -record(state,
        { tid :: ets:tab(),
@@ -17,6 +30,10 @@
 -define(PATH, "identify/1.0.0").
 -define(TIMEOUT, 5000).
 
+%% ------------------------------------------------------------------
+%% API Function Definitions
+%% ------------------------------------------------------------------
+
 -spec dial_spawn(Session::pid(), ets:tab(), Handler::pid()) -> pid().
 dial_spawn(Session, TID, Handler) ->
     spawn(fun() ->
@@ -25,12 +42,18 @@ dial_spawn(Session, TID, Handler) ->
                   libp2p_session:dial_framed_stream(Path, Session, ?MODULE, [TID, Handler])
           end).
 
+%% @hidden
 client(Connection, Args=[_TID, _Handler]) ->
     libp2p_framed_stream:client(?MODULE, Connection, Args).
 
+%% @hidden
 server(Connection, Path, TID, []) ->
     libp2p_framed_stream:server(?MODULE, Connection, [Path, TID]).
 
+%% ------------------------------------------------------------------
+%% libp2p_framed_stream Function Definitions
+%% ------------------------------------------------------------------
+%% @hidden
 init(client, Connection, [_TID, Handler]) ->
     case libp2p_connection:session(Connection) of
         {ok, Session} ->
@@ -52,13 +75,13 @@ init(server, Connection, [Path, TID]) ->
                                         SigFun),
     {stop, normal, libp2p_identify:encode(Identify)}.
 
-
+%% @hidden
 handle_data(client, Data, State=#state{}) ->
     erlang:cancel_timer(State#state.timeout),
     State#state.handler ! {handle_identify, State#state.session, libp2p_identify:decode(Data)},
     {stop, normal, State}.
 
-
+%% @hidden
 handle_info(client, identify_timeout, State=#state{}) ->
     State#state.handler ! {handle_identify, State#state.session, {error, timeout}},
     lager:notice("Identify timed out"),
