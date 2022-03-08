7ac4cfde87462f3f53d0394af84237186f654120
Andrew Thompson <andrew@hijacked.us>
2022-03-08 10:35:05 -0800

    Convert identify to enif protobuf

--- src/identify/libp2p_identify.erl
+++ src/identify/libp2p_identify.erl
@@ -3,6 +3,7 @@
 -include("pb/libp2p_peer_pb.hrl").
 -include("pb/libp2p_identify_pb.hrl").
 
+
 -type identify() :: #libp2p_signed_identify_pb{}.
 -type identify_map() :: #{ peer => libp2p_peer:peer(),
                            observed_addr => string(),
@@ -12,13 +13,15 @@
 -export([from_map/2, encode/1, decode/1, verify/1,
          pubkey_bin/1, peer/1, observed_maddr/1, observed_addr/1, nonce/1]).
 
+-on_load(load_pb_msg_defs/0).
+
 -spec from_map(identify_map(), libp2p_crypto:sig_fun()) -> identify().
 from_map(Map, SigFun) ->
     Identify = #libp2p_identify_pb{peer=maps:get(peer, Map),
                                    observed_addr=multiaddr:new(maps:get(observed_addr, Map)),
                                    nonce=maps:get(nonce, Map)
                                   },
-    Signature = SigFun(libp2p_identify_pb:encode_msg(Identify)),
+    Signature = SigFun(enif_protobuf:encode(Identify)),
     #libp2p_signed_identify_pb{identify=Identify, signature=Signature}.
 
 -spec peer(identify()) -> libp2p_peer:peer().
@@ -43,13 +46,13 @@ nonce(#libp2p_signed_identify_pb{identify=#libp2p_identify_pb{nonce=Nonce}}) ->
 %% @doc Encodes the given identify into its binary form.
 -spec encode(identify()) -> binary().
 encode(Msg=#libp2p_signed_identify_pb{}) ->
-    libp2p_identify_pb:encode_msg(Msg).
+    enif_protobuf:encode(Msg).
 
 %% @doc Decodes a given binary into an identify.
 -spec decode(binary()) -> {ok, identify()} | {error, term()}.
 decode(Bin) ->
     try
-        Msg = libp2p_identify_pb:decode_msg(Bin, libp2p_signed_identify_pb),
+        Msg = enif_protobuf:decode(Bin, libp2p_signed_identify_pb),
         verify(Msg)
     catch
         _:_ -> {error, invalid_binary}
@@ -58,9 +61,13 @@ decode(Bin) ->
 %% @doc Cryptographically verifies a given identify.
 -spec verify(identify()) -> {ok, identify()} | {error, term()}.
 verify(Msg=#libp2p_signed_identify_pb{identify=Ident=#libp2p_identify_pb{}, signature=Signature}) ->
-    EncodedIdentify = libp2p_identify_pb:encode_msg(Ident),
+    EncodedIdentify = enif_protobuf:encode(Ident),
     PubKey = libp2p_crypto:bin_to_pubkey(pubkey_bin(Msg)),
     case libp2p_crypto:verify(EncodedIdentify, Signature, PubKey) of
         true -> {ok, Msg};
         false -> {error, invalid_signature}
     end.
+
+load_pb_msg_defs() ->
+    ok = enif_protobuf:load_cache(libp2p_identify_pb:get_proto_defs()),
+    enif_protobuf:set_opts([{string_as_list, true}]).
